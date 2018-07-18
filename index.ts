d22a613534876e582d8cc56b0f12ded80fba9ab5
Sergey Shandar <sergey.shandar@gmail.com>
2018-07-18 14:45:11 -0700

    visit is simplified

--- index.ts
+++ index.ts
@@ -8,34 +8,24 @@ export type Object = object & ObjectProperties
 // ReadonlyArray<Json>
 export type ArrayObject = Array<any>
 
-export type Json = null|boolean|string|number|Object|ArrayObject
+export type Json = null|boolean|string|number|ArrayObject|Object
 
 export interface Visitor<T> {
     asNull(): T
     asBoolean(value: boolean): T
     asString(value: string): T
     asNumber(value: number): T
-    asObject(value: ObjectProperties): T
     asArray(value: ReadonlyArray<Json>): T
+    asObject(value: ObjectProperties): T
 }
 
 export function visit<T>(value: Json, visitor: Visitor<T>) {
-    if (value === null) {
-        return visitor.asNull()
-    }
-    if (typeof value === "boolean") {
-        return visitor.asBoolean(value)
-    }
-    if (typeof value === "string") {
-        return visitor.asString(value)
-    }
-    if (typeof value === "number") {
-        return visitor.asNumber(value)
-    }
-    if (value instanceof Array) {
-        return visitor.asArray(value)
-    }
-    return visitor.asObject(value)
+    return value === null ? visitor.asNull() :
+        typeof value === "boolean" ? visitor.asBoolean(value) :
+        typeof value === "string" ? visitor.asString(value) :
+        typeof value === "number" ? visitor.asNumber(value) :
+        value instanceof Array ? visitor.asArray(value):
+        visitor.asObject(value)
 }
 
 export const parse: (str: string) => Json = JSON.parse
