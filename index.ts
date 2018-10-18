a1638dbb6bc75249768a6d47c11a1562a961031e
Sergey Shandar <sergey.shandar@gmail.com>
2018-10-18 11:20:19 -0700

    minor

--- index.ts
+++ index.ts
@@ -30,14 +30,13 @@ export interface Visitor<T> {
     asObject(value: JsonObject): T
 }
 
-export function visit<T>(value: Json, visitor: Visitor<T>) {
-    return value === null ? visitor.asNull() :
-        typeof value === "boolean" ? visitor.asBoolean(value) :
-        typeof value === "string" ? visitor.asString(value) :
-        typeof value === "number" ? visitor.asNumber(value) :
-        _.isArray(value) ? visitor.asArray(value) :
-        visitor.asObject(value)
-}
+export const visit = <T>(value: Json, visitor: Visitor<T>) =>
+    value === null ? visitor.asNull() :
+    typeof value === "boolean" ? visitor.asBoolean(value) :
+    typeof value === "string" ? visitor.asString(value) :
+    typeof value === "number" ? visitor.asNumber(value) :
+    _.isArray(value) ? visitor.asArray(value) :
+    visitor.asObject(value)
 
 export const parse: (str: string) => Json = JSON.parse
 
